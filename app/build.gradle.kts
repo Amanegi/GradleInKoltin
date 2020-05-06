73a66d38421e60bd1d86098c14feb5646c60e8c3
Aman <amannegi229@gmail.com>
2020-05-06 15:58:35 +0530

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
     id("kotlin-android-extensions")
     id("kotlin-kapt")
 }
-// koltin kapt is used in place of annotationProcessor
+// kotlin kapt is used in place of annotationProcessor
 
 android {
     compileSdkVersion(29)
-    buildToolsVersion = "29.0.2"
+    buildToolsVersion = "29.0.3"
     defaultConfig {
-        applicationId = "com.aman.gradleinkotlindemo"
+        applicationId = "com.aman.gradleinkotlindemo" //change package name here
         minSdkVersion(23)
         targetSdkVersion(29)
         versionCode = 1
@@ -30,9 +30,9 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.61")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.72")
     implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.core:core-ktx:1.1.0")
+    implementation("androidx.core:core-ktx:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
     testImplementation("junit:junit:4.13")
     androidTestImplementation("androidx.test.ext:junit:1.1.1")
